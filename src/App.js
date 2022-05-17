2baffc87d0b45d4de9cfe5182bc1670c0c69c4ef
mdaias <mdaias29@gmail.com>
2022-05-17 23:04:25 +0600

    login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home/Home';
+import Login from './pages/Login/Login/Login';
+import Registration from './pages/Login/Registration/Registration';
+import Footer from './pages/Shared/Footer/Footer';
+import Header from './pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
         <h1>Spice Warehouse</h1>
+        <Header></Header>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+          <Route path='/registration'element={<Registration></Registration>}></Route>
+        </Routes>
+        <Footer></Footer>
     </div>
   );
 }
--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -3,7 +3,23 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h1>Footer</h1>
+            <footer class="text-center text-white" style={{backgroundColor: '#0a4275'}}>
+                <div class="container p-6">
+                    <div class="">
+                        <p class="flex justify-center items-center">
+                            <span class="mr-4">Register for free</span>
+                            <button type="button" class="inline-block px-6 py-2 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
+                                Sign up!
+                            </button>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="text-center p-4" style={{backgroundColor: 'rgba(0, 0, 0, 0.2)'}}>
+                    © 2021 Copyright:
+                    <a class="text-white" href="https://tailwind-elements.com/">Tailwind Elements</a>
+                </div>
+            </footer>
         </div>
     );
 };
