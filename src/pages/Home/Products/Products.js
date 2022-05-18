70d21d1effb61876a5124c4c40fd353c9d3623f0
mdaias <mdaias29@gmail.com>
2022-05-19 04:37:20 +0600

    added a section in home

--- src/pages/Home/Products/Products.js
+++ src/pages/Home/Products/Products.js
@@ -13,9 +13,10 @@ const Products = () => {
     }, [])
 
     return (
-        <div>
-            <h1 className='mx-5 text-center text-4xl font-bold my-5'>Our Products {products.length}</h1>
-            <div className='md:mx-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5'>
+        <div className='mt-10 md:mx-24 md:mt-20 mb-10'>
+            <h1 className='mx-5 text-center font-bold text-xl md:text-6xl mb-2'>Garden <span className=' text-orange-600'>Fresh Picks</span>, Just For You</h1>
+            <h3 className='text-xs md:text-3xl font-bold text-center md:mb-16 mb-10'>Our Products Guarantee A Healthy And Tasty Meal</h3>
+            <div className='md:mx-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
                 {
                     products.map(product => <Product key={product._id} product={product}></Product>)
                 }
